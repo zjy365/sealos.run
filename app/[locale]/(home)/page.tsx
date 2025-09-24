331862a4d126588f5083bf4ee040e508ebcd4ad5
Nixieboluo <me@sagirii.me>
2025-09-24 18:20:28 +0800

    chore: clean up for sealos.run
    Signed-off-by: Nixieboluo <me@sagirii.me>

--- app/[locale]/(home)/page.tsx
+++ app/[locale]/(home)/page.tsx
@@ -1,15 +1,7 @@
 import Image from 'next/image';
 import { Footer } from '@/libs/components/Footer';
 import { Navbar } from '@/libs/components/Navbar';
-import { CarouselSection } from './components/CarouselSection';
-import { CTASection } from './components/CTASection';
-import { FAQSection } from './components/FAQSection';
-import { Hero } from './components/Hero';
-import { PartnersSection } from './components/PartnersSection';
-import { WhoSection } from './components/WhoSection';
-import { WhySection } from './components/WhySection';
 import FooterHexagonalPattern from './images/footer-hexagonal-pattern.svg';
-import HeaderHexagonalPattern from './images/header-hexagonal-pattern.svg';
 
 export default async function HomePage() {
 	return (
@@ -18,27 +10,9 @@ export default async function HomePage() {
 				<Navbar />
 			</header>
 
-			<main className='relative'>
-				<div className='absolute top-0 right-0'>
-					<Image
-						src={HeaderHexagonalPattern}
-						alt=''
-						role='presentation'
-					/>
-				</div>
-
-				<Hero />
-
-				<CarouselSection />
-				<WhySection />
-				<WhoSection />
-				<FAQSection />
-				<PartnersSection />
-			</main>
+			<main className='relative'></main>
 
 			<footer className='relative'>
-				<CTASection />
-
 				<Footer />
 
 				<div className='pointer-events-none absolute bottom-0 w-full'>
