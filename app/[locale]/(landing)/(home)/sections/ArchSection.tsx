5e5ba024f8f62b985a39b50532101e0e7c73d80a
Nixieboluo <me@sagirii.me>
2026-04-03 18:32:11 +0800

    feat: new arch section
    Signed-off-by: Nixieboluo <me@sagirii.me>

--- app/[locale]/(landing)/(home)/sections/ArchSection.tsx
+++ app/[locale]/(landing)/(home)/sections/ArchSection.tsx
@@ -1,3 +1,17 @@
+import Image from 'next/image';
+import { cn } from '@/libs/utils/styling';
+import AppsArchImage from '../assets/arch-images/apps.svg';
+import AppstoreArchImage from '../assets/arch-images/appstore.svg';
+import MaintainingArchImage from '../assets/arch-images/maintaining.svg';
+import PlatformArchImage from '../assets/arch-images/platform.svg';
+
+const archImages = [
+	{ src: MaintainingArchImage, alt: 'Maintaining architecture' },
+	{ src: AppstoreArchImage, alt: 'Appstore architecture' },
+	{ src: AppsArchImage, alt: 'Apps architecture' },
+	{ src: PlatformArchImage, alt: 'Platform architecture' },
+];
+
 export function ArchSection() {
 	return (
 		<div className='flex w-full flex-col items-start gap-14'>
@@ -10,8 +24,28 @@ export function ArchSection() {
 				</p>
 			</div>
 
-			<div className='w-full max-w-full overflow-x-scroll'>
-				<div className='flex w-full min-w-5xl flex-col'></div>
+			<div className='mx-auto w-full max-w-6xl'>
+				{archImages.map((image, index) => {
+					const isRightAligned = (index + 1) % 2 === 0;
+
+					return (
+						<div
+							key={image.alt}
+							className={cn(
+								'max-w-[581px]',
+								index === 2 && 'max-w-[851px]',
+								index > 0 && 'lg:-mt-12',
+								isRightAligned && 'justify-self-end',
+							)}
+						>
+							<Image
+								src={image.src}
+								alt={image.alt}
+								className={cn('h-auto')}
+							/>
+						</div>
+					);
+				})}
 			</div>
 		</div>
 	);
